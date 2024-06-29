6dcc93db41781ab7874345ea771671de0443a621
hungnk <nguyenkyhunght@gmail.com>
2024-06-29 15:29:01 +0700

    feat: handle error

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,4 +12,20 @@ router.use(permission("0000"));
 router.use("/v1/api", require("./access"));
 router.use("/v1/api", require("./shop.route"));
 
+// handling error
+router.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+router.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  return res.status(statusCode).json({
+    status: "error",
+    code: statusCode,
+    message: error.message || "Internal Server Error",
+  });
+});
+
 module.exports = router;
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -5,6 +5,7 @@ const shopModel = require("../models/shop.model");
 const KeyTokenService = require("./keytoken.service");
 const { createTokenPairs } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
+const { BadRequestError } = require("../core/error.response");
 
 const RoleShop = {
   SHOP: "SHOP",
@@ -15,66 +16,50 @@ const RoleShop = {
 
 class AccessService {
   static signUp = async ({ name, email, password }) => {
-    try {
-      // Check email exist or not
-      const holderShop = await shopModel.findOne({ email }).lean();
-      if (holderShop) {
-        return {
-          code: "xxx",
-          message: "Shop already registered",
-        };
-      }
-      const hashPassword = await bcrypt.hash(password, 10);
+    // Check email exist or not
+    const holderShop = await shopModel.findOne({ email }).lean();
+    if (holderShop) {
+      throw new BadRequestError("Error: Shop already registed!");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
 
-      const newShop = await shopModel.create({
-        name,
-        email,
-        password: hashPassword,
-        roles: [RoleShop.SHOP],
+    const newShop = await shopModel.create({
+      name,
+      email,
+      password: hashPassword,
+      roles: [RoleShop.SHOP],
+    });
+
+    if (newShop) {
+      const publicKey = crypto.randomBytes(64).toString("hex");
+      const privateKey = crypto.randomBytes(64).toString("hex");
+
+      console.table({ publicKey, privateKey });
+      const keyStores = await KeyTokenService.createKeyToken({
+        userId: newShop._id,
+        publicKey,
+        privateKey,
       });
 
-      if (newShop) {
-        const publicKey = crypto.randomBytes(64).toString("hex");
-        const privateKey = crypto.randomBytes(64).toString("hex");
-
-        console.table({ publicKey, privateKey });
-        const keyStores = await KeyTokenService.createKeyToken({
-          userId: newShop._id,
-          publicKey,
-          privateKey,
-        });
-
-        if (!keyStores) {
-          return {
-            code: "xxx",
-            message: "keyStores error",
-          };
-        }
-
-        const tokens = await createTokenPairs(
-          { userId: newShop._id, email },
-          publicKey,
-          privateKey,
-        );
-
+      if (!keyStores) {
         return {
-          code: 201,
-          metadata: {
-            shop: getInfoData({ fields: ["name", "_id"], object: newShop }),
-            tokens,
-          },
+          code: "xxx",
+          message: "keyStores error",
         };
       }
 
+      const tokens = await createTokenPairs(
+        { userId: newShop._id, email },
+        publicKey,
+        privateKey,
+      );
+
       return {
-        code: 200,
-        data: null,
-      };
-    } catch (error) {
-      return {
-        code: 123,
-        status: "error",
-        message: error.message,
+        code: 201,
+        metadata: {
+          shop: getInfoData({ fields: ["name", "_id"], object: newShop }),
+          tokens,
+        },
       };
     }
   };
